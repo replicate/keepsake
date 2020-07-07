eb9a3a0e897838a8276c42f586ea5ca44da77c25
Ben Firshman <ben@firshman.co.uk>
2020-07-07 15:52:06 -0700

    Move example into its own directory

--- example/example.py
+++ example/example.py
@@ -3,7 +3,8 @@ import replicate
 
 experiment = replicate.init(params={"learning_rate": 0.002})
 
-print("Starting experiment " + experiment.id)
+print("Starting experiment {}...".format(experiment.id))
 
 for epoch in range(10):
+    print("Committing epoch {}...".format(epoch))
     experiment.commit(metrics={"epoch": epoch})
