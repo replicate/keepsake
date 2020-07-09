0144bc0153f79364d73217cb5ea7fa00279f19e0
Ben Firshman <ben@firshman.co.uk>
2020-07-08 18:18:18 -0700

    Fix loading empty replicate.yaml in Python

--- python/tests/unit/test_config.py
+++ python/tests/unit/test_config.py
@@ -1,6 +1,17 @@
 import pytest  # type: ignore
 
-from replicate.config import validate_and_set_defaults, ConfigValidationError
+from replicate.config import (
+    load_config,
+    validate_and_set_defaults,
+    ConfigValidationError,
+)
+
+
+def test_load_config_blank(tmp_path):
+    config_file = tmp_path / "replicate.yaml"
+    config_file.write_text("")
+
+    assert load_config(tmp_path) == {"storage": ".replicate/storage/"}
 
 
 def test_validate():
