6b9027578de9abfb541d61cb66feca8619f23c50
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2020-07-23 00:57:54 +0200

    heartbeat background process

---
+++ python/replicate/heartbeat.py
@@ -0,0 +1,61 @@
+import sys
+import datetime
+import json
+import time
+from multiprocessing import Process
+
+from .storage import storage_for_url, Storage
+from .metadata import rfc3339_datetime
+
+
+DEFAULT_REFRESH_INTERVAL = datetime.timedelta(seconds=10)
+
+
+class Heartbeat:
+    def __init__(
+        self,
+        storage_url: str,
+        path: str,
+        refresh_interval: datetime.timedelta = DEFAULT_REFRESH_INTERVAL,
+    ):
+        self.storage_url = storage_url
+        self.path = path
+        self.refresh_interval = refresh_interval
+        self.process = self.make_process()
+
+    def start(self):
+        self.process.start()
+
+    def ensure_running(self):
+        if not self.is_alive():
+            self.process = self.make_process()
+            self.process.start()
+
+    def kill(self):
+        self.process.terminate()
+
+    def is_alive(self):
+        return self.process.is_alive()
+
+    def make_process(self) -> Process:
+        process = Process(target=self.heartbeat_loop)
+        process.daemon = True
+        return process
+
+    def heartbeat_loop(self):
+        # need to instantitate storage here since the gcs
+        # client doesn't like multiprocessing:
+        # https://github.com/googleapis/google-cloud-python/issues/3501
+        storage = storage_for_url(self.storage_url)
+        while True:
+            self.refresh(storage)
+            time.sleep(self.refresh_interval.total_seconds())
+
+    def refresh(self, storage: Storage):
+        obj = json.dumps(
+            {"last_heartbeat": rfc3339_datetime(datetime.datetime.utcnow())}
+        )
+        try:
+            storage.put(self.path, obj)
+        except Exception as e:
+            sys.stderr.write("Failed to save checkpoint, got error: {}".format(e))
