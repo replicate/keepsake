126b47e41473c3bd313f4916d7ccb0a4db00c2e1
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2020-07-24 22:30:43 +0200

    clarify unread experiment fields

--- cli/pkg/experiment/experiment.go
+++ cli/pkg/experiment/experiment.go
@@ -19,13 +19,15 @@ var heartbeatMissTolerance = 3
 
 // Experiment represents a training run
 type Experiment struct {
-	ID            string                  `json:"id"`
-	Created       time.Time               `json:"created"`
-	Params        map[string]*param.Value `json:"params"`
-	Host          string                  `json:"host"`
-	User          string                  `json:"user"`
-	LastHeartbeat time.Time               `json:"last_heartbeat"`
-	Running       bool                    `json:"running"`
+	ID      string                  `json:"id"`
+	Created time.Time               `json:"created"`
+	Params  map[string]*param.Value `json:"params"`
+	Host    string                  `json:"host"`
+	User    string                  `json:"user"`
+
+	// properties that are not actually part of metadata json
+	LastHeartbeat time.Time `json:"-"`
+	Running       bool      `json:"-"`
 }
 
 // NewExperiment creates a commit, setting ID and Created
