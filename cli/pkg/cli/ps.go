90937a44a6a22222d8c436711bd508d844cf951d
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2020-07-29 02:07:28 +0200

    --storage-url flag in replicate ls/ps

--- cli/pkg/cli/ps.go
+++ cli/pkg/cli/ps.go
@@ -1,15 +1,9 @@
 package cli
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/spf13/cobra"
 
-	"replicate.ai/cli/pkg/config"
-	"replicate.ai/cli/pkg/global"
 	"replicate.ai/cli/pkg/list"
-	"replicate.ai/cli/pkg/slices"
 	"replicate.ai/cli/pkg/storage"
 )
 
@@ -19,40 +13,27 @@ func newPsCommand() *cobra.Command {
 		Short:   "List running experiments in this project",
 		Aliases: []string{"processes"},
 		RunE:    listRunningExperiments,
-		Args:    cobra.ExactArgs(0),
+		Args:    cobra.NoArgs,
 	}
 
-	cmd.Flags().StringP("format", "f", "table", "Output format (table/json)")
+	addStorageURLFlag(cmd)
+	addListFormatFlag(cmd)
 
 	return cmd
 }
 
 func listRunningExperiments(cmd *cobra.Command, args []string) error {
-	var storageURL string
-	// FIXME (bfirsh): perhaps better as a flag? (just putting this here to remind ourselves to have this discussion)
-	if len(args) == 1 {
-		storageURL = args[0]
-	} else {
-		conf, _, err := config.FindConfigInWorkingDir(global.SourceDirectory)
-		if err != nil {
-			return err
-		}
-		storageURL = conf.Storage
-	}
-
-	format, err := cmd.Flags().GetString("format")
+	storageURL, _, err := getStorageURLFromFlagOrConfig(cmd)
 	if err != nil {
 		return err
 	}
-	validFormats := []string{list.FormatJSON, list.FormatTable}
-	if !slices.ContainsString(validFormats, format) {
-		return fmt.Errorf("%s is not a valid format. Valid formats are: %s", format, strings.Join(validFormats, ", "))
+	format, err := parseListFormatFlag(cmd)
+	if err != nil {
+		return err
 	}
-
 	store, err := storage.ForURL(storageURL)
 	if err != nil {
 		return err
 	}
-
 	return list.RunningExperiments(store, format)
 }
