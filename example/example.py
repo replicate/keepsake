c4c744477c4f248465e82c605ccbab2beea3da2b
Ben Firshman <ben@firshman.co.uk>
2020-08-03 18:09:07 -0700

    Update example to use new API

--- example/example.py
+++ example/example.py
@@ -5,11 +5,11 @@ import replicate
 
 print(torch.cuda.device_count())
 
-experiment = replicate.init(params={"learning_rate": 0.002})
+experiment = replicate.init(learning_rate=0.002)
 
 print("Starting experiment {}...".format(experiment.id))
 
 for epoch in range(10):
     print("Committing epoch {}...".format(epoch))
-    experiment.commit(step=epoch)
-    time.sleep(100)
+    experiment.commit(step=epoch, accuracy="0.987")
+    time.sleep(1)
