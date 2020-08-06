e12af5fafc2c453a1b57aef7433a7906f2279bab
Ben Firshman <ben@firshman.co.uk>
2020-08-06 11:00:03 -0700

    Release Python 0.0.9

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 # fmt: off
 setuptools.setup(
     name="replicate",
-    version="0.0.8",
+    version="0.0.9",
     author="",
     author_email="",
     description="",
