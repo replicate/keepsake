065021a54eda94697296dbf195613002596a29a0
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2020-08-14 16:45:20 +0200

    Bump to version 0.1.6

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Replicate",
   url: "https://beta.replicate.ai",
   customFields: {
-    version: "0.1.5",
+    version: "0.1.6",
   },
   baseUrl: "/",
   favicon: "img/favicon.ico",
