02e67b881d1f2d2a3139735ff0068d710441396a
Ben Firshman <ben@firshman.co.uk>
2020-08-13 17:40:24 -0700

    Hide unfinished docs

--- web/sidebars.js
+++ web/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Tutorial: ["tutorial", "working-with-remote-machines"],
-    Guides: ["how-it-works"],
-    Reference: ["example-models", "python", "replicate-yaml", "cli"],
+    // Guides: ["how-it-works"],
+    Reference: ["example-models", /*"python",*/ "replicate-yaml", "cli"],
   },
 };
