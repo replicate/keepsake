0ec260388fb364a611a688584ec0e720f088a7d1
Ben Firshman <ben@firshman.co.uk>
2020-09-08 08:27:56 -0700

    Bump to version 0.1.16

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Replicate",
   url: "https://beta.replicate.ai",
   customFields: {
-    version: "0.1.15",
+    version: "0.1.16",
   },
   baseUrl: "/",
   favicon: "img/favicon.ico",
