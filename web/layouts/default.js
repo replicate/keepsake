ebf0e5e495391b1a231d9725c130c8684061318f
Ben Firshman <ben@firshman.co.uk>
2020-10-08 09:42:53 -0700

    Add us to website

--- web/layouts/default.js
+++ web/layouts/default.js
@@ -1,4 +1,10 @@
 import Head from "next/head";
+import {
+  faGithub,
+  faTwitter,
+  faSoundcloud,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Layout({ title, children }) {
   return (
@@ -9,12 +15,15 @@ function Layout({ title, children }) {
       </Head>
 
       <div className="global-banner">
-        <p>Welcome to the Replicate private beta! If you have any feedback, or know somebody who might like it, email us:{" "}<a href="mailto:team@replicate.ai">team@replicate.ai</a></p>
+        <p>
+          Welcome to the Replicate private beta! If you have any feedback, or
+          know somebody who might like it, email us:{" "}
+          <a href="mailto:team@replicate.ai">team@replicate.ai</a>
+        </p>
         <nav>
           <a href="/docs">Docs</a>
           <a href="https://github.com/replicate/replicate">GitHub</a>
         </nav>
-
       </div>
 
       <div className="layout">
@@ -31,35 +40,40 @@ function Layout({ title, children }) {
               <a href="/docs">learn more about how Replicate works</a>
             </div>
           </h2>
-          {/* <div id="contributors">
-            <h3>Contributors</h3>
+          <div id="contributors">
+            <h3>Made by</h3>
             <ul>
               <li>
-                <img src="assets/img/photo.png" />
-                <h4>Name Surname</h4>
+                <img src="/images/ben.jpg" />
+                <h4>Ben Firshman</h4>
+                <p>Product at Docker, creator of Docker&nbsp;Compose.</p>
                 <p>
-                  A brief biog or some details about what they have done on the
-                  project. <a href="#">More</a>
+                  <a href="https://github.com/bfirsh" className="link">
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                  <a href="https://twitter.com/bfirsh" className="link">
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </a>
                 </p>
               </li>
               <li>
-                <img src="assets/img/photo.png" />
-                <h4>Name Surname</h4>
+                <img src="/images/andreas.jpg" />
+                <h4>Andreas Jansson</h4>
+                <p>ML infrastructure lead &amp; researcher at Spotify.</p>
                 <p>
-                  A brief biog or some details about what they have done on the
-                  project. <a href="#">More</a>
-                </p>
-              </li>
-              <li>
-                <img src="assets/img/photo.png" />
-                <h4>Name Surname</h4>
-                <p>
-                  A brief biog or some details about what they have done on the
-                  project. <a href="#">More</a>
+                  <a href="https://github.com/andreasjansson" className="link">
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
                 </p>
               </li>
             </ul>
+            <p>
+              Together, we also made{" "}
+              <a href="https://www.arxiv-vanity.com/">arXiv Vanity</a>, which
+              lets you read arXiv papers as responsive web pages.
+            </p>
           </div>
+          {/* 
           <div id="get-involved">
             <h3>Get involved</h3>
             <p>
