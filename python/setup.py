bb990f1376a9c4787d8c071b61b5f83affab0f5f
Ben Firshman <ben@firshman.co.uk>
2020-10-21 19:18:30 -0700

    Bump to version 0.1.33

--- python/setup.py
+++ python/setup.py
@@ -110,7 +110,7 @@ class develop(_develop):
 # fmt: off
 setuptools.setup(
     name="replicate",
-    version="0.1.32",
+    version="0.1.33",
     author="",
     author_email="",
     description="",
