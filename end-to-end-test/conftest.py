8bb9f3cdd7545e028bd5a886f01c6b940b3ac89c
Ben Firshman <ben@firshman.co.uk>
2020-10-27 10:52:38 -0700

    Remove gpu_instance for end to end tests

--- end-to-end-test/conftest.py
+++ end-to-end-test/conftest.py
@@ -1,85 +1,10 @@
 import socket
 import time
-from dataclasses import dataclass
 import string
 import random
-import subprocess
 import pytest
-import paramiko
 import boto3
-from botocore.config import Config
 from botocore.exceptions import ClientError, NoCredentialsError
-from mypy_boto3_ec2 import EC2ServiceResource
-from mypy_boto3_ec2.service_resource import Instance
-
-
-@dataclass
-class TempInstance:
-    public_ip: str
-    ssh_private_key: str
-    username: str
-    aws_access_key_id: str
-    aws_secret_access_key: str
-
-    def exec_output(self, cmdline):
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(
-            self.public_ip_address,
-            username=self.username,
-            key_filename=self.ssh_private_key,
-        )
-
-        stdin, stdout, stderr = ssh.exec_command(cmdline)
-        return stdout.read(), stderr.read()
-
-
-def pytest_addoption(parser):
-    parser.addoption("--aws-access-key-id", action="store")
-    parser.addoption("--aws-secret-access-key", action="store")
-    parser.addoption("--ssh-private-key", action="store")
-
-
-@pytest.fixture(scope="function")
-def gpu_instance(request):
-    # FIXME (bfirsh): just use environment variables? they are needed for S3 anyway
-    aws_access_key_id = request.config.getoption("--aws-access-key-id")
-    aws_secret_access_key = request.config.getoption("--aws-secret-access-key")
-    ssh_private_key = request.config.getoption("--ssh-private-key")
-    ami_id = "ami-0800ac2bbf9b818db"  # deep learning AMI (ami-0507b1ad768ac009d) + us.gcr.io/replicate/base-ubuntu18.04-python3.7-cuda10.1-cudnn7-pytorch1.4.0:0.3 already pulled
-    username = "ubuntu"
-
-    session = boto3.Session(
-        region_name="us-east-1",
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-    )
-
-    print("Creating instance")
-
-    ec2: EC2ServiceResource = session.resource("ec2")
-    # TODO(andreas): run ci instance in separate subnet for isolation?
-    instance: Instance = ec2.create_instances(
-        ImageId=ami_id, MaxCount=1, MinCount=1, InstanceType="p2.xlarge", KeyName="ci",
-    )[0]
-    try:
-        instance.wait_until_running()
-        instance.reload()
-
-        print("Waiting for instance to become accessible")
-        wait_for_port(host=instance.public_ip_address, port=22, timeout=120.0)
-
-        yield TempInstance(
-            public_ip=instance.public_ip_address,
-            ssh_private_key=ssh_private_key,
-            username=username,
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,
-        )
-    finally:
-        instance.terminate()
-        # save some time by not calling
-        # instance.wait_until_terminated()
 
 
 @pytest.fixture(scope="function")
