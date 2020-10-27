6ee3178c51dc65bdfca0f00a1aa72fbaa000cfd9
Ben Firshman <ben@firshman.co.uk>
2020-10-27 13:58:10 -0700

    Run GCS tests with a single bucket
    It rate limits bucket creation.

--- cli/pkg/storage/gcs_test.go
+++ cli/pkg/storage/gcs_test.go
@@ -47,6 +47,19 @@ func deleteGCSBucket(t *testing.T, bucket *storage.BucketHandle) {
 	require.NoError(t, bucket.Delete(context.Background()))
 }
 
+func clearGCSBucket(t *testing.T, bucket *storage.BucketHandle) {
+	it := bucket.Objects(context.Background(), &storage.Query{
+		Prefix: "",
+	})
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		require.NoError(t, bucket.Object(attrs.Name).Delete(context.Background()))
+	}
+}
+
 func createObject(t *testing.T, bucket *storage.BucketHandle, key string, data []byte) {
 	obj := bucket.Object(key)
 	w := obj.NewWriter(context.Background())
@@ -64,107 +77,97 @@ func readObject(t *testing.T, bucket *storage.BucketHandle, key string) []byte {
 	return data
 }
 
-func TestGCSStorageGet(t *testing.T) {
-	client, err := storage.NewClient(context.TODO())
-	require.NoError(t, err)
-	bucket, bucketName := createGCSBucket(t, client)
-	t.Cleanup(func() { deleteGCSBucket(t, bucket) })
-	createObject(t, bucket, "foo.txt", []byte("hello"))
-
-	storage, err := NewGCSStorage(bucketName, "")
-	require.NoError(t, err)
-	data, err := storage.Get("foo.txt")
-	require.NoError(t, err)
-	require.Equal(t, []byte("hello"), data)
-}
-
-func TestGCSGetPathTar(t *testing.T) {
+// Run tests all in one go with one bucket because GCS rate limits bucket creation
+func TestGCSStorage(t *testing.T) {
 	client, err := storage.NewClient(context.TODO())
 	require.NoError(t, err)
 	bucket, bucketName := createGCSBucket(t, client)
 	t.Cleanup(func() { deleteGCSBucket(t, bucket) })
 
-	storage, err := NewGCSStorage(bucketName, "")
-	require.NoError(t, err)
+	t.Run("Get", func(t *testing.T) {
+		createObject(t, bucket, "foo.txt", []byte("hello"))
+		storage, err := NewGCSStorage(bucketName, "")
+		require.NoError(t, err)
+		data, err := storage.Get("foo.txt")
+		require.NoError(t, err)
+		require.Equal(t, []byte("hello"), data)
+	})
 
-	tmpDir, err := files.TempDir("test")
-	require.NoError(t, err)
-	err = storage.GetPathTar("does-not-exist.tar.gz", tmpDir)
-	require.IsType(t, &DoesNotExistError{}, err)
-}
+	clearGCSBucket(t, bucket)
 
-func TestGCSStoragePut(t *testing.T) {
-	client, err := storage.NewClient(context.TODO())
-	require.NoError(t, err)
-	bucket, bucketName := createGCSBucket(t, client)
-	t.Cleanup(func() { deleteGCSBucket(t, bucket) })
+	t.Run("GetPathTar", func(t *testing.T) {
+		storage, err := NewGCSStorage(bucketName, "")
+		require.NoError(t, err)
 
-	storage, err := NewGCSStorage(bucketName, "")
-	require.NoError(t, err)
-	err = storage.Put("foo.txt", []byte("hello"))
-	require.NoError(t, err)
+		tmpDir, err := files.TempDir("test")
+		require.NoError(t, err)
+		err = storage.GetPathTar("does-not-exist.tar.gz", tmpDir)
+		require.IsType(t, &DoesNotExistError{}, err)
+	})
 
-	require.Equal(t, []byte("hello"), readObject(t, bucket, "foo.txt"))
-}
+	clearGCSBucket(t, bucket)
 
-func TestGCSStoragePutPath(t *testing.T) {
-	client, err := storage.NewClient(context.TODO())
-	require.NoError(t, err)
-	bucket, bucketName := createGCSBucket(t, client)
-	t.Cleanup(func() { deleteGCSBucket(t, bucket) })
+	t.Run("Put", func(t *testing.T) {
+		storage, err := NewGCSStorage(bucketName, "")
+		require.NoError(t, err)
+		err = storage.Put("foo.txt", []byte("hello"))
+		require.NoError(t, err)
 
-	tmpDir, err := files.TempDir("test")
-	require.NoError(t, err)
-	defer os.RemoveAll(tmpDir)
-	err = os.Mkdir(path.Join(tmpDir, "somedir"), 0755)
-	require.NoError(t, err)
-	err = ioutil.WriteFile(path.Join(tmpDir, "somedir/foo.txt"), []byte("hello"), 0644)
-	require.NoError(t, err)
+		require.Equal(t, []byte("hello"), readObject(t, bucket, "foo.txt"))
+	})
 
-	storage, err := NewGCSStorage(bucketName, "")
-	require.NoError(t, err)
+	clearGCSBucket(t, bucket)
 
-	// Whole directory
-	err = storage.PutPath(filepath.Join(tmpDir, "somedir"), "anotherdir")
-	require.NoError(t, err)
-	require.Equal(t, []byte("hello"), readObject(t, bucket, "anotherdir/foo.txt"))
+	t.Run("PutPath", func(t *testing.T) {
+		tmpDir, err := files.TempDir("test")
+		require.NoError(t, err)
+		defer os.RemoveAll(tmpDir)
+		err = os.Mkdir(path.Join(tmpDir, "somedir"), 0755)
+		require.NoError(t, err)
+		err = ioutil.WriteFile(path.Join(tmpDir, "somedir/foo.txt"), []byte("hello"), 0644)
+		require.NoError(t, err)
 
-	// Single file
-	err = storage.PutPath(filepath.Join(tmpDir, "somedir/foo.txt"), "singlefile/foo.txt")
-	require.NoError(t, err)
-	require.Equal(t, []byte("hello"), readObject(t, bucket, "singlefile/foo.txt"))
+		storage, err := NewGCSStorage(bucketName, "")
+		require.NoError(t, err)
 
-}
+		// Whole directory
+		err = storage.PutPath(filepath.Join(tmpDir, "somedir"), "anotherdir")
+		require.NoError(t, err)
+		require.Equal(t, []byte("hello"), readObject(t, bucket, "anotherdir/foo.txt"))
 
-func TestGCSListRecursive(t *testing.T) {
-	client, err := storage.NewClient(context.TODO())
-	require.NoError(t, err)
-	bucket, bucketName := createGCSBucket(t, client)
-	t.Cleanup(func() { deleteGCSBucket(t, bucket) })
+		// Single file
+		err = storage.PutPath(filepath.Join(tmpDir, "somedir/foo.txt"), "singlefile/foo.txt")
+		require.NoError(t, err)
+		require.Equal(t, []byte("hello"), readObject(t, bucket, "singlefile/foo.txt"))
+	})
 
-	storage, err := NewGCSStorage(bucketName, "")
-	require.NoError(t, err)
+	clearGCSBucket(t, bucket)
 
-	// Works with empty storage
-	results := make(chan ListResult)
-	go storage.ListRecursive(results, "checkpoints")
-	require.Empty(t, <-results)
+	t.Run("ListRecursive", func(t *testing.T) {
+		storage, err := NewGCSStorage(bucketName, "")
+		require.NoError(t, err)
 
-	// Lists stuff!
-	require.NoError(t, storage.Put("checkpoints/abc123.json", []byte("yep")))
-	require.NoError(t, storage.Put("experiments/def456.json", []byte("nope")))
-	results = make(chan ListResult)
-	go storage.ListRecursive(results, "checkpoints")
-	require.Equal(t, ListResult{
-		Path: "checkpoints/abc123.json",
-		MD5:  []byte{0x93, 0x48, 0xae, 0x78, 0x51, 0xcf, 0x3b, 0xa7, 0x98, 0xd9, 0x56, 0x4e, 0xf3, 0x8, 0xec, 0x25},
-	}, <-results)
-	require.Empty(t, <-results)
+		// Works with empty storage
+		results := make(chan ListResult)
+		go storage.ListRecursive(results, "checkpoints")
+		require.Empty(t, <-results)
 
-	// Works with non-existent bucket
-	storage, err = NewGCSStorage("replicate-test-"+hash.Random()[0:10], "")
-	require.NoError(t, err)
-	results = make(chan ListResult)
-	go storage.ListRecursive(results, "checkpoints")
-	require.Empty(t, <-results)
+		// Lists stuff!
+		require.NoError(t, storage.Put("checkpoints/abc123.json", []byte("yep")))
+		require.NoError(t, storage.Put("experiments/def456.json", []byte("nope")))
+		results = make(chan ListResult)
+		go storage.ListRecursive(results, "checkpoints")
+		require.Equal(t, ListResult{
+			Path: "checkpoints/abc123.json",
+			MD5:  []byte{0x93, 0x48, 0xae, 0x78, 0x51, 0xcf, 0x3b, 0xa7, 0x98, 0xd9, 0x56, 0x4e, 0xf3, 0x8, 0xec, 0x25},
+		}, <-results)
+		require.Empty(t, <-results)
+
+		// Works with non-existent bucket
+		storage, err = NewGCSStorage("replicate-test-"+hash.Random()[0:10], "")
+		require.NoError(t, err)
+		results = make(chan ListResult)
+		go storage.ListRecursive(results, "checkpoints")
+		require.Empty(t, <-results)
+	})
 }
