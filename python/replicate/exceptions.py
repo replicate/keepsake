da14b2c596270248b1c8438d595915ee5ca323f4
Ben Firshman <ben@firshman.co.uk>
2020-10-30 13:42:41 -0700

    Fix repository documentation

--- python/replicate/exceptions.py
+++ python/replicate/exceptions.py
@@ -13,8 +13,8 @@ class ConfigNotFoundError(Exception):
     def __init__(self, message):
         # TODO(andreas): global DOC_URL constant
         message += """
-You must either create a replicate.yaml configuration file,
-or explicitly pass the arguments 'repository' and 'directory' to
-replicate.Project().
-For more information, see https://beta.replicate.ai/docs"""
+
+You must either create a replicate.yaml configuration file, or explicitly pass the arguments 'repository' and 'directory' to replicate.Project().
+
+For more information, see https://replicate.ai/docs/reference/yaml"""
         super().__init__(message)
