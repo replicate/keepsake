a7cbc3b943dbdc2c68f9a1db0a62a5bdaa9af20e
Ben Firshman <ben@firshman.co.uk>
2020-11-14 14:51:28 -0800

    Second take at manifesto

--- web/components/footer.js
+++ web/components/footer.js
@@ -7,43 +7,52 @@ function Footer({ children }) {
     <footer>
       {children}
       <div id="manifesto" className="body">
-        <h2>Let's build this together</h2>
+        <h2>Let’s build this together</h2>
         <p>
-          Everyone uses version control for software, but it's much less common
-          in machine learning. Why is this?
+          Everyone uses version control for software, but it’s much less common
+          in machine learning.
         </p>
         <p>
-          We spent a year talking to people in the ML community and this is what
-          we found out:
+          This causes all sorts of problems: people are manually keeping track
+          of things in spreadsheets, model weights are scattered on S3, and
+          results can’t be reproduced. Somebody who wrote a model has left the
+          team? Bad luck – nothing’s written down and you’ve probably got to
+          start from scratch.
+        </p>
+        <p>
+          So why isn’t everyone using Git?{" "}
+          <strong>Git doesn’t work well with machine learning.</strong> It can’t
+          handle large files, it can’t handle key/value metadata like metrics,
+          and it can’t record information automatically from inside a training
+          script. There are some solutions for these things, but they feel like
+          a band-aid.
+        </p>
+        <p>
+          We spent a year talking to people in the ML community about this, and
+          this is what we found out:
         </p>
         <ul>
           <li>
-            <strong>Git doesn’t work well with machine learning.</strong> It
-            can’t handle large files, it can’t handle key/value metadata like
-            metrics, and it can’t commit automatically in your training script.
-            There are some solutions for this, but they feel like band-aids.
-          </li>
-          <li>
-            <strong>It should be open source.</strong> There are a number of
-            proprietary solutions, but something so foundational needs to be
-            built by and for the ML community.
+            <strong>We need a native version control system for ML.</strong>{" "}
+            It’s sufficiently different to normal software that we can’t just
+            put band-aids on existing systems.
           </li>
           <li>
             <strong>It needs to be small, easy to use, and extensible.</strong>{" "}
-            We found people struggling to integrate with “AI Platforms”. We want
-            to make a tool that does one thing well and can be combined with
-            other tools to produce the system you need.
+            We found people struggling to migrate to “AI Platforms”. We believe
+            tools should do one thing well and combine with other tools to
+            produce the system you need.
+          </li>
+          <li>
+            <strong>It needs to be open source.</strong> There are a number of
+            proprietary solutions, but something so foundational needs to be
+            built by and for the ML community.
           </li>
         </ul>
         <p>
-          We think the ML community needs a good version control system. But,
-          version control systems are complex, and to make this a reality we
-          need your help.
-        </p>
-        <p>
-          Have you strung together some shell scripts to build this for
-          yourself? Are you interested in the problem of making machine learning
-          reproducible?
+          We need your help to make this a reality. If you’ve built this for
+          yourself, or are just interested in this problem, join us to help
+          build a better system for everyone.
         </p>
         <p>
           <a className="button" href="https://discord.gg/QmzJApGjyE">
