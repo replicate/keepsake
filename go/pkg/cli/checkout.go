55b1a29d8eadc00896a06268ff5017a35440def3
Aditya Paliwal <adityapaliwal95@gmail.com>
2020-12-17 19:29:06 +0530

    Add flag --path to command checkout to allow checking out individual files or directories
    Signed-off-by: Aditya Paliwal <adityapaliwal95@gmail.com>

--- go/pkg/cli/checkout.go
+++ go/pkg/cli/checkout.go
@@ -19,6 +19,7 @@ type checkoutOpts struct {
 	outputDirectory string
 	force           bool
 	repositoryURL   string
+	checkoutPath    string
 }
 
 func newCheckoutCommand() *cobra.Command {
@@ -36,92 +37,71 @@ func newCheckoutCommand() *cobra.Command {
 	addRepositoryURLFlagVar(cmd, &opts.repositoryURL)
 	cmd.Flags().StringVarP(&opts.outputDirectory, "output-directory", "o", "", "Output directory (defaults to working directory or directory with replicate.yaml in it)")
 	cmd.Flags().BoolVarP(&opts.force, "force", "f", false, "Force checkout without prompt, even if the directory is not empty")
+	cmd.Flags().StringVarP(&opts.checkoutPath, "path", "", "", "A specific file or directory to checkout (defaults to all files or directory in checkpoint/experiment)")
 
 	return cmd
 }
 
-func checkoutCheckpoint(opts checkoutOpts, args []string) error {
-	prefix := args[0]
-
-	outputDir := opts.outputDirectory
-	if outputDir == "" {
-		var err error
-		outputDir, err = getProjectDir()
-		if err != nil {
-			return err
-		}
-	}
-
+// Returns the repository requested by opts.repositoryURL
+func getRepositoryFromOpts(opts checkoutOpts) (repository.Repository, error) {
 	repositoryURL, projectDir, err := getRepositoryURLFromStringOrConfig(opts.repositoryURL)
 	if err != nil {
-		return err
+		return nil, err
 	}
+
 	repo, err := getRepository(repositoryURL, projectDir)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	exists, err := files.FileExists(outputDir)
-	if err != nil {
-		return err
-	}
-	if exists {
-		isDir, err := files.IsDir(outputDir)
-		if err != nil {
-			return err
-		}
-		if !isDir {
-			return fmt.Errorf("Checkout path %q is not a directory", outputDir)
-		}
-	} else {
-		if err := os.MkdirAll(outputDir, 0755); err != nil {
-			return fmt.Errorf("Failed to create directory %q: %w", outputDir, err)
-		}
-	}
+	return repo, nil
+}
 
+// Returns the experiment and the most appropriate checkpoint for that experiment.
+func getExperimentAndCheckpoint(prefix string, repo repository.Repository) (*project.Experiment, *project.Checkpoint, error) {
 	proj := project.NewProject(repo)
 	result, err := proj.CheckpointOrExperimentFromPrefix(prefix)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
-
 	experiment := result.Experiment
 	checkpoint := result.Checkpoint
 
 	if checkpoint != nil {
 		console.Info("Checking out files from checkpoint %s and its experiment %s", checkpoint.ShortID(), experiment.ShortID())
-	} else {
-		// When checking out experiment, also check out best/latest checkpoint
-		checkpoint = experiment.BestCheckpoint()
-		if checkpoint != nil {
-			console.Info("Checking out files from experiment %s and its best checkpoint %s", experiment.ShortID(), checkpoint.ShortID())
-		} else {
-			checkpoint = experiment.LatestCheckpoint()
-			if checkpoint != nil {
-				console.Info("Checking out files from experiment %s and its latest checkpoint %s", experiment.ShortID(), checkpoint.ShortID())
-			} else {
-				console.Info("Checking out files from experiment %s", experiment.ShortID())
-			}
-		}
+		return experiment, checkpoint, nil
 	}
 
-	displayPath := filepath.Join(outputDir, experiment.Path)
-
-	// FIXME(bfirsh): this is a bodge and isn't always quite right -- if no experiment path set, and we're checking out checkpoint, display the checkpoint path
-	if experiment.Path == "" && checkpoint != nil {
-		displayPath = filepath.Join(outputDir, checkpoint.Path)
+	// When checking out experiment, also check out best/latest checkpoint
+	checkpoint = experiment.BestCheckpoint()
+	if checkpoint != nil {
+		console.Info("Checking out files from experiment %s and its best checkpoint %s", experiment.ShortID(), checkpoint.ShortID())
+		return experiment, checkpoint, nil
 	}
 
-	exists, err = files.FileExists(displayPath)
+	checkpoint = experiment.LatestCheckpoint()
+	if checkpoint != nil {
+		console.Info("Checking out files from experiment %s and its latest checkpoint %s", experiment.ShortID(), checkpoint.ShortID())
+		return experiment, checkpoint, nil
+	}
+
+	console.Info("Checking out files from experiment %s", experiment.ShortID())
+	return experiment, checkpoint, nil
+}
+
+// Prompt user for confirmation if the diplsayPath already exists
+func overwriteDisplayPathPrompt(displayPath string, force bool) error {
+	exists, err := files.FileExists(displayPath)
 	if err != nil {
 		return err
 	}
+
 	if exists {
 		isEmpty, err := files.DirIsEmpty(displayPath)
 		if err != nil {
 			return err
 		}
-		if !isEmpty && !opts.force {
+		if !isEmpty && !force {
 			console.Warn("The directory %q is not empty.", displayPath)
 			console.Warn("%s Make sure they're saved in Git or Replicate so they're safe!", aurora.Bold("This checkout may overwrite existing files."))
 			fmt.Println()
@@ -139,9 +119,79 @@ func checkoutCheckpoint(opts checkoutOpts, args []string) error {
 			}
 		}
 	}
+	return nil
+}
+
+// replicate CLI `checkout` command
+func checkoutCheckpoint(opts checkoutOpts, args []string) error {
+	prefix := args[0]
+
+	repo, err := getRepositoryFromOpts(opts)
+	if err != nil {
+		return err
+	}
+
+	experiment, checkpoint, err := getExperimentAndCheckpoint(prefix, repo)
+	if err != nil {
+		return err
+	}
+
+	outputDir := opts.outputDirectory
+	if outputDir == "" {
+		var err error
+		outputDir, err = getProjectDir()
+		if err != nil {
+			return err
+		}
+	}
+
+	// TODO(adipal): There is no way to distinguish if the outputPath is a file or
+	// a directory. Ask the user for a prompt if the checkoutPath is a file?
+	exists, err := files.FileExists(outputDir)
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		if err := os.MkdirAll(outputDir, 0755); err != nil {
+			return fmt.Errorf("Failed to create directory %q: %w", outputDir, err)
+		}
+	}
+
+	isDir, err := files.IsDir(outputDir)
+	if err != nil {
+		return err
+	}
+
+	if !isDir {
+		return fmt.Errorf("Checkout path %q is not a directory", outputDir)
+	}
+
+	displayPath := filepath.Join(outputDir, experiment.Path)
+
+	// FIXME(bfirsh): this is a bodge and isn't always quite right -- if no experiment path set, and we're checking out checkpoint, display the checkpoint path
+	if experiment.Path == "" && checkpoint != nil {
+		displayPath = filepath.Join(outputDir, checkpoint.Path)
+	}
+
+	err = overwriteDisplayPathPrompt(displayPath, opts.force)
+	if err != nil {
+		return err
+	}
 
 	fmt.Fprintln(os.Stderr)
 
+	checkoutPath := opts.checkoutPath
+	if checkoutPath == "" {
+		return checkoutEntireCheckpoint(outputDir, repo, experiment, checkpoint)
+	} else {
+		return checkoutFileOrDir(outputDir, checkoutPath, repo, experiment, checkpoint)
+	}
+}
+
+// checkout all the files from an experiment or checkpoint
+func checkoutEntireCheckpoint(outputDir string, repo repository.Repository, experiment *project.Experiment, checkpoint *project.Checkpoint) error {
+	// Extract the tarfile
 	experimentFilesExist := true
 	checkpointFilesExist := true
 
@@ -188,4 +238,57 @@ func checkoutCheckpoint(opts checkoutOpts, args []string) error {
 `)
 
 	return nil
+
+}
+
+// checkout all the files from an experiment or checkpoint
+func checkoutFileOrDir(outputDir string, checkoutPath string, repo repository.Repository, experiment *project.Experiment, checkpoint *project.Checkpoint) error {
+	// Extract the tarfile
+	experimentFilesExist := true
+	checkpointFilesExist := true
+
+	if err := repo.GetPathItemTar(path.Join("experiments", experiment.ID+".tar.gz"), checkoutPath, outputDir); err != nil {
+		// Ignore does not exist errors
+		if _, ok := err.(*repository.DoesNotExistError); ok {
+			console.Debug("No experiment data found")
+			experimentFilesExist = false
+		} else {
+			return err
+		}
+	} else {
+		console.Info("Copied the files from experiment %s to %q", experiment.ShortID(), filepath.Join(outputDir, experiment.Path))
+	}
+
+	// Overlay checkpoint on top of experiment
+	if checkpoint != nil {
+
+		if err := repo.GetPathItemTar(path.Join("checkpoints", checkpoint.ID+".tar.gz"), checkoutPath, outputDir); err != nil {
+			if _, ok := err.(*repository.DoesNotExistError); ok {
+				console.Debug("No checkpoint data found")
+				checkpointFilesExist = false
+			} else {
+				return err
+
+			}
+		} else {
+			console.Info("Copied the files from checkpoint %s to %q", checkpoint.ShortID(), filepath.Join(outputDir, checkpoint.Path))
+		}
+
+	}
+
+	if !experimentFilesExist && !checkpointFilesExist {
+		// Just an experiment, no checkpoints
+		if checkpoint == nil {
+			return fmt.Errorf("The experiment %s does not have any files associated with it. You need to pass the 'path' argument to 'init()' to check out files.", experiment.ShortID())
+		}
+		return fmt.Errorf("Neither the experiment %s nor the checkpoint %s has any files associated with it. You need to pass the 'path' argument to 'init()' or 'checkpoint()' to check out files.", experiment.ShortID(), checkpoint.ShortID())
+	}
+
+	console.Info(`If you want to run this experiment again, this is how it was run:
+
+  ` + experiment.Command + `
+`)
+
+	return nil
+
 }
