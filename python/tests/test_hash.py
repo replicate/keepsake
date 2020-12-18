bd32da667a104601ef45053f30f3b93ae69aaae6
Andreas Jansson <andreas@replicate.ai>
2020-12-18 13:40:15 +0100

    include time in ID-generating hashing function
    fixes https://github.com/replicate/replicate/issues/417
    
    Signed-off-by: Andreas Jansson <andreas@replicate.ai>

---
+++ python/tests/test_hash.py
@@ -0,0 +1,25 @@
+import random
+
+from replicate.hash import random_hash
+
+
+def test_hash_length():
+    assert len(random_hash(0)) == 0
+    assert len(random_hash(1)) == 1
+    assert len(random_hash(63)) == 63
+    assert len(random_hash(64)) == 64
+    assert len(random_hash(65)) == 65
+    assert len(random_hash(1000)) == 1000
+
+
+def test_hash_random_seed():
+    random.seed(0)
+    h1 = random_hash()
+    random.seed(0)
+    h2 = random_hash()
+    assert h1 != h2
+
+
+def test_immediate_hash():
+    h1, h2 = random_hash(), random_hash()
+    assert h1 != h2
