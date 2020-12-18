c66db684070b0758ad46bc5f72a456756e5b9162
Andreas Jansson <andreas@replicate.ai>
2020-12-18 13:40:15 +0100

    include time in ID-generating hashing function
    fixes https://github.com/replicate/replicate/issues/417
    
    Signed-off-by: Andreas Jansson <andreas@replicate.ai>

--- python/replicate/hash.py
+++ python/replicate/hash.py
@@ -1,5 +1,17 @@
+import hashlib
+import time
 import random
 
 
 def random_hash(length=64) -> str:
-    return "".join(random.choice("0123456789abcdef") for i in range(length))
+    # time-dependent hash to begin with, then pad with random characters
+    hasher = hashlib.sha256()
+    hasher.update(str(time.time()).encode("utf-8"))
+    hasher.update(str(random.random()).encode("utf-8"))
+    digest = hasher.hexdigest()
+    if len(digest) >= length:
+        return digest[:length]
+    padding = "".join(
+        random.choice("0123456789abcdef") for i in range(length - len(digest))
+    )
+    return digest + padding
