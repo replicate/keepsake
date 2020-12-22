7dfa6a4ea1f63c4c97c90a333a44f14f677ba6cc
Ben Firshman <ben@firshman.co.uk>
2020-12-22 15:11:14 -0800

    Build and test on Windows
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- python/setup.py
+++ python/setup.py
@@ -24,6 +24,7 @@ with open("../README.md", "r", encoding="utf-8") as fh:
 # https://packaging.python.org/specifications/platform-compatibility-tags/
 # https://www.python.org/dev/peps/pep-0425/
 PLAT_NAME_TO_BINARY_PATH = {
+    "win_amd64": "windows/amd64",
     "linux_x86_64": "linux/amd64",
     # "linux" is the default if no --plat-name is passed, but it is not specific
     # enough for pypi, so we use manylinux for the released version
