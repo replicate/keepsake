7fd4344b79c12e4aa781918bd1940c5c5ffd8d6b
Ben Firshman <ben@firshman.co.uk>
2021-02-05 12:43:59 -0800

    Update footer
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- web/components/footer.js
+++ web/components/footer.js
@@ -28,29 +28,13 @@ function Footer({ children }) {
             feel like band-aids.
           </p>
           <p>
-            We spent a year talking to people in the ML community about this,
-            and this is what we found out:
+            <strong>
+              We want to make a small, lightweight, native version control
+              system for ML.
+            </strong>{" "}
+            Something that does one thing well and combines with other tools to
+            produce the system you need.
           </p>
-          <ul>
-            <li>
-              <strong>We need a native version control system for ML.</strong>{" "}
-              It’s sufficiently different to normal software that we can’t just
-              put band-aids on existing systems.
-            </li>
-            <li>
-              <strong>
-                It needs to be small, easy to use, and extensible.
-              </strong>{" "}
-              We found people struggling to migrate to “AI Platforms”. We
-              believe tools should do one thing well and combine with other
-              tools to produce the system you need.
-            </li>
-            <li>
-              <strong>It needs to be open source.</strong> There are a number of
-              proprietary solutions, but something so foundational needs to be
-              built by and for the ML community.
-            </li>
-          </ul>
           <p>
             We need your help to make this a reality. If you’ve built this for
             yourself, or are just interested in this problem, join us to help
@@ -101,70 +85,14 @@ function Footer({ children }) {
           </form>
         </div>
       </div>
-      <div id="contributors">
-        <h3>Core team</h3>
-        <div className="us">
-          <figure>
-            <div
-              style={{ backgroundImage: "url(" + "/images/ben.jpg" + ")" }}
-            ></div>
-            <figcaption>
-              <h4>Ben Firshman</h4>
-              <p>Product at Docker, creator of Docker&nbsp;Compose.</p>
-              <p>
-                <a href="https://github.com/bfirsh" className="link">
-                  <FontAwesomeIcon icon={faGithub} />
-                </a>
-                <a href="https://twitter.com/bfirsh" className="link">
-                  <FontAwesomeIcon icon={faTwitter} />
-                </a>
-              </p>
-            </figcaption>
-          </figure>
-          <figure>
-            <div
-              style={{
-                backgroundImage: "url(" + "/images/andreas.jpg" + ")",
-              }}
-            ></div>
-            <figcaption>
-              <h4>Andreas Jansson</h4>
-              <p>
-                ML infrastructure and research at Spotify. PhD in ML for music.
-              </p>
-              <p>
-                <a href="https://github.com/andreasjansson" className="link">
-                  <FontAwesomeIcon icon={faGithub} />
-                </a>
-              </p>
-            </figcaption>
-          </figure>
-        </div>
-        <div className="more">
-          <p>
-            We also built{" "}
-            <a href="https://www.arxiv-vanity.com/" target="_blank">
-              arXiv Vanity
-            </a>
-            , which lets you read arXiv papers as responsive web pages.
-          </p>
-        </div>
-      </div>
+      <p className="project-from">
+        A project from <a href="https://replicate.ai">Replicate</a>.
+      </p>
       <nav>
-        <Link href="/docs">
-          <a>Docs</a>
-        </Link>
         <a href="https://github.com/replicate/keepsake">GitHub</a>
         <a href="https://twitter.com/replicateai">Twitter</a>
         <a href="mailto:team@replicate.ai">team@replicate.ai</a>
       </nav>
-      <p className="tagline">
-        <Link href="/">
-          <a>
-            <strong>Keepsake</strong> Version control for machine&nbsp;learning
-          </a>
-        </Link>
-      </p>
     </footer>
   );
 }
