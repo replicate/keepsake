512b792090dc8ff2b5321bf33c2046296e749bb6
Enoch Kan <kanxx030@gmail.com>
2021-03-08 09:44:28 +0000

    removed keepsake.yaml and added temp_dir
    Signed-off-by: Enoch Kan <kanxx030@gmail.com>

--- python/tests/test_plot.py
+++ python/tests/test_plot.py
@@ -5,23 +5,26 @@ from keepsake.checkpoint import Checkpoint, CheckpointList
 from keepsake.experiment import Experiment, ExperimentList
 from keepsake.project import Project, init
 
-experiment = init(path=".", params={"learning_rate": 0.1, "num_epochs": 1},)
 
-experiment.checkpoint(
-    path=".",
-    step=1,
-    metrics={"loss": 1.1836304664611816, "accuracy": 0.3333333432674408},
-    primary_metric=("loss", "minimize"),
-)
-experiment.checkpoint(
-    path=".",
-    step=2,
-    metrics={"loss": 1.1836304662222222, "accuracy": 0.4333333432674408},
-    primary_metric=("loss", "minimize"),
-)
+def test_num_plots(temp_workdir):
+    with open("keepsake.yaml", "w") as f:
+        f.write("repository: file://.keepsake/")
 
+    experiment = init(path=".", params={"learning_rate": 0.1, "num_epochs": 1},)
 
-def test_num_plots():
+    experiment.checkpoint(
+        path=".",
+        step=1,
+        metrics={"loss": 1.1836304664611816, "accuracy": 0.3333333432674408},
+        primary_metric=("loss", "minimize"),
+    )
+    experiment.checkpoint(
+        path=".",
+        step=2,
+        metrics={"loss": 1.1836304662222222, "accuracy": 0.4333333432674408},
+        primary_metric=("loss", "minimize"),
+    )
+
     experiment_list = ExperimentList([experiment])
     num_plots = 30
     for rep in range(num_plots):
